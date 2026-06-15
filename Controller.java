f7dcd07844e67f5c2778818f0236066facfc425c
sva <chanvanchho987@gmail.com>
2026-06-15 16:04:07 +0700

    u

--- Controller.java
+++ Controller.java
@@ -2,6 +2,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+
 
 public class Controller {
 
@@ -12,13 +19,30 @@ public class Controller {
     private URL location;
 
     @FXML
+    private ScrollPane scrollPane;
+
+    @FXML
     void on_submit(ActionEvent event) {
 
     }
 
+    VBox vbox = new VBox();
+
+    HBox hbox_1 = new HBox();
+    HBox hbox_2 = new HBox();
+    HBox hbox_3 = new HBox();
+
+    Label label_1 = new Label("Item 1");
+    Label label_1_price = new Label("5.00$");
+    ImageView image_1 = new ImageView(getClass().getResource("Arduino.jpg").toExternalForm());
+    Spinner<Integer> spinner_1 = new Spinner<>(0, 10, 0);
+
     @FXML
     void initialize() {
+        hbox_1.getChildren().addAll(image_1, label_1, label_1_price, spinner_1);
+        vbox.getChildren().addAll(hbox_1);
 
+        scrollPane.setContent(vbox);
     }
 
 }
