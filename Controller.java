63b356483f3e044c4982ffb296b39e1451d9a2f4
sva <chanvanchho987@gmail.com>
2026-06-15 16:19:41 +0700

    U

--- Controller.java
+++ Controller.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.ImageView;
 
-
 public class Controller {
 
     @FXML
@@ -29,29 +28,28 @@ public class Controller {
 
     VBox vbox = new VBox();
 
-    HBox hbox_1 = new HBox();
-    HBox hbox_2 = new HBox();
-    HBox hbox_3 = new HBox();
+    // HBox hbox_1 = new HBox();
+    // HBox hbox_2 = new HBox();
+    // HBox hbox_3 = new HBox();
 
-    Label label_1 = new Label("Item 1");
-    Label label_1_price = new Label("5.00$");
-    ImageView image_1 = new ImageView("Arduino.jpg");
-    Spinner<Integer> spinner_1 = new Spinner<>(0, 10, 0);
+    // Label label_1 = new Label("Item 1");
+    // Label label_1_price = new Label("5.00$");
+    // ImageView image_1 = new ImageView("Arduino.jpg");
+    // Spinner<Integer> spinner_1 = new Spinner<>(0, 10, 0);
 
-    Pos pos = Pos.CENTER; 
-
+    // Pos pos = Pos.CENTER;
 
     @FXML
     void initialize() {
 
-        image_1.setFitWidth(100);
-        image_1.setFitHeight(100);
+        // image_1.setFitWidth(100);
+        // image_1.setFitHeight(100);
 
-        hbox_1.getChildren().addAll(image_1, label_1, label_1_price, spinner_1);
+        // hbox_1.getChildren().addAll(image_1, label_1, label_1_price, spinner_1);
 
-        hbox_1.setAlignment(pos);
+        // hbox_1.setAlignment(pos);
 
-        vbox.getChildren().addAll(hbox_1);
+        // vbox.getChildren().addAll(hbox_1);
 
         scrollPane.setContent(vbox);
     }
