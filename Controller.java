1e865f09f63520e5d6014bc7ee476fe2a0627aba
sva <chanvanchho987@gmail.com>
2026-06-15 16:32:31 +0700

    u

--- Controller.java
+++ Controller.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.input.MouseEvent;
 
 public class Controller {
 
@@ -25,20 +24,13 @@ public class Controller {
     private ScrollPane scrollpane;
 
     @FXML
-    void initialize(MouseEvent event) {
-
-    }
-
-    @FXML
     void on_submit(ActionEvent event) {
 
     }
 
     @FXML
     void initialize() {
-        assert button_submit != null : "fx:id=\"button_submit\" was not injected: check your FXML file 'View.fxml'.";
-        assert label_total != null : "fx:id=\"label_total\" was not injected: check your FXML file 'View.fxml'.";
-        assert scrollpane != null : "fx:id=\"scrollpane\" was not injected: check your FXML file 'View.fxml'.";
+       
 
     }
 
