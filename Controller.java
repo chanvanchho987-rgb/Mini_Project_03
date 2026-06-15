4e165a4d543c7043b7e2ddadbce5587d4d88fada
sva <chanvanchho987@gmail.com>
2026-06-15 15:45:36 +0700

    u

--- Controller.java
+++ Controller.java
@@ -1,5 +1,6 @@
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 public class Controller {
@@ -9,6 +10,11 @@ public class Controller {
 
     @FXML
     private URL location;
+
+    @FXML
+    void on_submit(ActionEvent event) {
+
+    }
 
     @FXML
     void initialize() {
